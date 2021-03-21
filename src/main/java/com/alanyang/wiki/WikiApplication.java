b894910aab82a9fd3f76fb74ce85566e336a1353
chingyuany <chingyy@g.clemson.edu>
2021-03-21 14:13:16 +0800

    Add banner change startup log back

--- src/main/java/com/alanyang/wiki/WikiApplication.java
+++ src/main/java/com/alanyang/wiki/WikiApplication.java
@@ -1,13 +1,26 @@
 package com.alanyang.wiki;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
 
 @SpringBootApplication
 public class WikiApplication {
-
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+//        SpringApplication.run(WikiApplication.class, args);
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("Start up success！！");
+        LOG.info("Address: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
