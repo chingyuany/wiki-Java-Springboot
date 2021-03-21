6cce9bfb6003927136260960b9ea80d327ecaa27
chingyuany <chingyy@g.clemson.edu>
2021-03-21 15:06:37 +0800

    Add hello test port via test.http/ unit test

--- src/main/java/com/alanyang/wiki/controller/TestController.java
+++ src/main/java/com/alanyang/wiki/controller/TestController.java
@@ -1,15 +1,21 @@
 package com.alanyang.wiki.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 //返回字串 or json
 @RestController
 public class TestController {
 //    http://127.0.0.1:8880/hello
 //    get,post,put(update), delete
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello(){
         return "Hello World";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World,Post," + name;
     }
 }
