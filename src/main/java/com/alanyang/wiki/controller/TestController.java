303f8d184b6705f1a002e570d55d92d2a1b0c03c
chingyuany <chingyy@g.clemson.edu>
2021-03-21 16:13:17 +0800

    Add properties and yml files

--- src/main/java/com/alanyang/wiki/controller/TestController.java
+++ src/main/java/com/alanyang/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.alanyang.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 //返回字串 or json
 @RestController
 public class TestController {
+//    如果讀不到application properties裡面配置項的值 就會輸出預設值Test
+    @Value("${test.hello:Test}")
+    private String testhello;
 //    http://127.0.0.1:8880/hello
 //    get,post,put(update), delete
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World";
+        return "Hello World" + testhello;
     }
 
     @PostMapping("/hello/post")
