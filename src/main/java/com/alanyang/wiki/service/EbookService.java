cde999880c3a0f51f8c32e1ee22d5f1aeec2c978
chingyuany <chingyy@g.clemson.edu>
2021-03-22 20:11:41 +0800

    using copyutil class

--- src/main/java/com/alanyang/wiki/service/EbookService.java
+++ src/main/java/com/alanyang/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.alanyang.wiki.domain.EbookExample;
 import com.alanyang.wiki.mapper.EbookMapper;
 import com.alanyang.wiki.req.EbookReq;
 import com.alanyang.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.alanyang.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,13 +23,21 @@ public class EbookService {
         criteria.andNameLike("%"+req.getName()+"%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-//            spring 內建的複製來源物件到目標物件函數
-            BeanUtils.copyProperties(ebook,ebookResp);
-            respList.add(ebookResp);
-        }
+//        第一種寫法
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+//  //            EbookResp ebookResp = new EbookResp();
+//  //            spring 內建的複製來源物件到目標物件函數
+//  //            BeanUtils.copyProperties(ebook,ebookResp);
+//  //            用CopyUtil class的 object 複製
+//            EbookResp ebookResp = CopyUtil.copy(ebook,EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+
+//        用CopyUtil class的列表複製寫法
+        List<EbookResp> respList = CopyUtil.copyList(ebookList,EbookResp.class);
+
+
         return respList;
 
     }
