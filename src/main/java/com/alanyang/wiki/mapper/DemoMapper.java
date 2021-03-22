2eb369f9904bb80c2bee574423cb821109adbc14
chingyuany <chingyy@g.clemson.edu>
2021-03-22 16:17:46 +0800

    mybatis auto generator

---
+++ src/main/java/com/alanyang/wiki/mapper/DemoMapper.java
@@ -0,0 +1,22 @@
+package com.alanyang.wiki.mapper;
+
+import com.alanyang.wiki.domain.Demo;
+import com.alanyang.wiki.domain.DemoExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface DemoMapper {
+    long countByExample(DemoExample example);
+
+    int deleteByExample(DemoExample example);
+
+    int insert(Demo record);
+
+    int insertSelective(Demo record);
+
+    List<Demo> selectByExample(DemoExample example);
+
+    int updateByExampleSelective(@Param("record") Demo record, @Param("example") DemoExample example);
+
+    int updateByExample(@Param("record") Demo record, @Param("example") DemoExample example);
+}
