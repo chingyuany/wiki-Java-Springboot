edfffcc5e94d15677cc720e3c39b5444368f65e4
chingyuany <chingyy@g.clemson.edu>
2021-03-22 17:07:05 +0800

    add common response class and modify ebook class

--- src/main/java/com/alanyang/wiki/controller/EbookController.java
+++ src/main/java/com/alanyang/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.alanyang.wiki.controller;
 
 import com.alanyang.wiki.domain.Ebook;
+import com.alanyang.wiki.resp.CommonResp;
 import com.alanyang.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,10 @@ public class EbookController {
 
 
     @GetMapping("/list")
-    public List<Ebook> list(){
-        return ebookService.list();
+    public CommonResp list(){
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
