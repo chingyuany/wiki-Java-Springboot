b3e6b60e11609ca5772f91223caf767d60a6c790
chingyuany <chingyy@g.clemson.edu>
2021-03-23 13:09:54 +0800

    Add ant design of vue and test button

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+//#app 是index.html裡面的 div 區塊
+createApp(App).use(store).use(router).use(Antd).mount('#app');
