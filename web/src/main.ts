5c9b497ddb1df09000406cbced6978166b861c75
chingyuany <chingyy@g.clemson.edu>
2021-03-23 20:43:57 +0800

    remove rule: data type any Add list module import icons in global use

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,5 +4,14 @@ import router from './router'
 import store from './store'
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/antd.css";
+//import ant 圖標庫
+import * as Icons from '@ant-design/icons-vue';
 //#app 是index.html裡面的 div 區塊
-createApp(App).use(store).use(router).use(Antd).mount('#app');
+const app = createApp(App);
+app.use(store).use(router).use(Antd).mount('#app');
+
+// 全局使用图标 不用一個一個import
+const icons: any = Icons;
+for (const i in icons) {
+    app.component(i, icons[i]);
+}
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,66 +1,118 @@
 <template>
   <a-layout>
-      <a-layout-sider width="200" style="background: #fff">
-        <a-menu
-            mode="inline"
-            v-model:selectedKeys="selectedKeys2"
-            v-model:openKeys="openKeys"
-            :style="{ height: '100%', borderRight: 0 }"
-        >
-          <a-sub-menu key="sub1">
-            <template #title>
+    <a-layout-sider width="200" style="background: #fff">
+      <a-menu
+          mode="inline"
+          v-model:selectedKeys="selectedKeys2"
+          v-model:openKeys="openKeys"
+          :style="{ height: '100%', borderRight: 0 }"
+      >
+        <a-sub-menu key="sub1">
+          <template #title>
                   <span>
                     <user-outlined />
                     subnav 1
                   </span>
-            </template>
-            <a-menu-item key="1">option1</a-menu-item>
-            <a-menu-item key="2">option2</a-menu-item>
-            <a-menu-item key="3">option3</a-menu-item>
-            <a-menu-item key="4">option4</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub2">
-            <template #title>
+          </template>
+          <a-menu-item key="1">option1</a-menu-item>
+          <a-menu-item key="2">option2</a-menu-item>
+          <a-menu-item key="3">option3</a-menu-item>
+          <a-menu-item key="4">option4</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <template #title>
                   <span>
                     <laptop-outlined />
                     subnav 2
                   </span>
-            </template>
-            <a-menu-item key="5">option5</a-menu-item>
-            <a-menu-item key="6">option6</a-menu-item>
-            <a-menu-item key="7">option7</a-menu-item>
-            <a-menu-item key="8">option8</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub3">
-            <template #title>
+          </template>
+          <a-menu-item key="5">option5</a-menu-item>
+          <a-menu-item key="6">option6</a-menu-item>
+          <a-menu-item key="7">option7</a-menu-item>
+          <a-menu-item key="8">option8</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub3">
+          <template #title>
                   <span>
                     <notification-outlined />
                     subnav 3
                   </span>
-            </template>
-            <a-menu-item key="9">option9</a-menu-item>
-            <a-menu-item key="10">option10</a-menu-item>
-            <a-menu-item key="11">option11</a-menu-item>
-            <a-menu-item key="12">option12</a-menu-item>
-          </a-sub-menu>
-        </a-menu>
-      </a-layout-sider>
-      <a-layout style="padding: 0 24px 24px">
+          </template>
+          <a-menu-item key="9">option9</a-menu-item>
+          <a-menu-item key="10">option10</a-menu-item>
+          <a-menu-item key="11">option11</a-menu-item>
+          <a-menu-item key="12">option12</a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout style="padding: 0 24px 24px">
 
-        <a-layout-content
-            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-        >
-          Content
-          <pre>{{ebooks}}</pre>
-          <pre>{{ebooksR}}</pre>
-        </a-layout-content>
-      </a-layout>
+      <a-layout-content
+          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+      >
+<!--下面是ant design vue 裡面的list代碼 https://2x.antdv.com/components/list-cn-->
+          <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+            <template #footer>
+              <div>
+                <b>ant design vue</b>
+                footer part
+              </div>
+            </template>
+            <template #renderItem="{ item }">
+              <a-list-item key="item.title">
+                <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+                </template>
+                <template #extra>
+                  <img
+                      width="272"
+                      alt="logo"
+                      src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                  />
+                </template>
+                <a-list-item-meta :description="item.description">
+                  <template #title>
+                    <a :href="item.href">{{ item.title }}</a>
+                  </template>
+                  <template #avatar><a-avatar :src="item.avatar" /></template>
+                </a-list-item-meta>
+                {{ item.content }}
+              </a-list-item>
+            </template>
+          </a-list>
+
+<!--ant design view的list 模板代碼-->
+
+
+
+      </a-layout-content>
+    </a-layout>
   </a-layout>
 </template>
+
+
 
 <script lang="ts">
 import { defineComponent,onMounted,ref,reactive,toRef } from 'vue';
 import axios from 'axios';
+
+// ant design view的list 模板代碼
+const listData: any = [];
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
+
 export default defineComponent({
   name: 'Home',
   //包含vue2的 data(各種變量), mounted and methods
@@ -87,7 +139,20 @@ export default defineComponent({
       //下面是ref的方法回傳
       ebooks,
       //reactive的方法回傳  最前面的ebooks是自己定義的變量 toRef是把所有屬性變成響應式變量   第一個參數是reactive的 const 變數, 第二個是裡面的屬性,
-      ebooksR:toRef(ebooksR,"books")
+      ebooksR:toRef(ebooksR,"books"),
+      // ant design view的list 模板代碼
+      listData,
+      pagination : {
+        onChange: (page: number) => {
+          console.log(page);
+        },
+        pageSize: 3,
+      },
+      actions:  [
+      { type: 'StarOutlined', text: '156' },
+      { type: 'LikeOutlined', text: '156' },
+      { type: 'MessageOutlined', text: '2' },
+    ],
     }
   }
 
