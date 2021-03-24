9c0e98b07218d5c64fd558104208b5cf41b29702
chingyuany <chingyy@g.clemson.edu>
2021-03-24 11:43:02 +0800

    Add log interceptor to record interface elapsed time

---
+++ src/main/java/com/alanyang/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,41 @@
+package com.alanyang.wiki.config;
+
+
+import com.alanyang.wiki.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor loginInterceptor;
+
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+//                先包含所有接口
+                .addPathPatterns("/**")
+//                在排除不需要攔截的街口
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/vote/**",
+                        "/doc/find-content/**",
+                        "/ebook-snapshot/**"
+                );
+
+//        registry.addInterceptor(actionInterceptor)
+//                .addPathPatterns(
+//                        "/*/save",
+//                        "/*/delete/**",
+//                        "/*/reset-password");
+    }
+}
