cffa126780156b25bdc33177bff3805870936c7f
chingyuany <chingyy@g.clemson.edu>
2021-03-29 16:23:47 +0800

    Add back end port for ebook snapshot

--- src/main/java/com/alanyang/wiki/mapper/EbookSnapshotMapperCust.java
+++ src/main/java/com/alanyang/wiki/mapper/EbookSnapshotMapperCust.java
@@ -1,13 +1,15 @@
 package com.alanyang.wiki.mapper;
 
-//import com.alanyang.wiki.resp.StatisticResp;
+import com.alanyang.wiki.resp.StatisticResp;
+
+import java.util.List;
 
 public interface EbookSnapshotMapperCust {
 
 
     public void genSnapshot();
-//
-//    List<StatisticResp> getStatistic();
+
+    List<StatisticResp> getStatistic();
 //
 //    List<StatisticResp> get30Statistic();
 }
--- src/main/java/com/alanyang/wiki/service/EbookSnapshotService.java
+++ src/main/java/com/alanyang/wiki/service/EbookSnapshotService.java
@@ -1,11 +1,11 @@
 package com.alanyang.wiki.service;
 
 import com.alanyang.wiki.mapper.EbookSnapshotMapperCust;
+import com.alanyang.wiki.resp.StatisticResp;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-
-//import com.alanyang.wiki.resp.StatisticResp;
+import java.util.List;
 
 @Service
 public class EbookSnapshotService {
@@ -17,12 +17,12 @@ public class EbookSnapshotService {
         ebookSnapshotMapperCust.genSnapshot();
     }
 
-//    /**
-//     * 获取首页数值数据：总阅读数、总点赞数、今日阅读数、今日点赞数、今日预计阅读数、今日预计阅读增长
-//     */
-//    public List<StatisticResp> getStatistic() {
-//        return ebookSnapshotMapperCust.getStatistic();
-//    }
+    /**
+     * 获取首页数值数据：总阅读数、总点赞数、今日阅读数、今日点赞数、今日预计阅读数、今日预计阅读增长
+     */
+    public List<StatisticResp> getStatistic() {
+        return ebookSnapshotMapperCust.getStatistic();
+    }
 //
 //    /**
 //     * 30天数值统计
