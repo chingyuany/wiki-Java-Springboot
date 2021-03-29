a35bd207098fd0eac2d178809c82b3fdd9cb8280
chingyuany <chingyy@g.clemson.edu>
2021-03-29 15:53:09 +0800

    add schedule job to update ebook snapshot

---
+++ src/main/java/com/alanyang/wiki/mapper/EbookSnapshotMapperCust.java
@@ -0,0 +1,13 @@
+package com.alanyang.wiki.mapper;
+
+//import com.alanyang.wiki.resp.StatisticResp;
+
+public interface EbookSnapshotMapperCust {
+
+
+    public void genSnapshot();
+//
+//    List<StatisticResp> getStatistic();
+//
+//    List<StatisticResp> get30Statistic();
+}
